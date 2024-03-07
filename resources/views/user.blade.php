9380040c40111a415c2c42137968c4e815ca4d07
Ahmed Fathir Syafaat <fathir123t@gmail.com>
2024-03-07 13:12:52 +0700

    Praktikum-2.3-Jobsheet-4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,21 +9,26 @@
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
+        {{-- <tr>
             <td>ID</td>
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
-        </tr>
+        </tr> --}}
         {{-- @foreach ($data as $d) --}}
-        <tr>
+        {{-- <tr>
             <td>{{$data->user_id}}</td>
             <td>{{$data->username}}</td>
             <td>{{$data->nama}}</td>
             <td>{{$data->level_id}}</td>
-        </tr>
+        </tr> --}}
         {{-- @endforeach --}}
-        
+        <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{$data}}</td>
+        </tr>
         
     </table>
 </body>
