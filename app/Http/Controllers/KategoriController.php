a4381d113fbad58057a29edb683d0ffaae563210
Ahmed Fathir Syafaat <fathir123t@gmail.com>
2024-03-16 14:23:24 +0700

    Praktikum-1-Jobsheet-5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,28 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\KategoriDataTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
-        // $data = [
-        //     'kategori_code' => 'SNK',
-        //     'kategori_nama' => 'Snack/Makanan Ringan',
-        //     'created_at' => now()
-        // ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
-
-        // $row = DB::table('m_kategori')->where('kategori_code', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
-
-        // $row = DB::table('m_kategori')->where('kategori_code', 'SNK')->delete();
-        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
-
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+        return $dataTable->render('kategori.index');
     }
+
+    // public function index()
+    // {
+    //     // $data = [
+    //     //     'kategori_code' => 'SNK',
+    //     //     'kategori_nama' => 'Snack/Makanan Ringan',
+    //     //     'created_at' => now()
+    //     // ];
+    //     // DB::table('m_kategori')->insert($data);
+    //     // return 'Insert data baru berhasil';
+
+    //     // $row = DB::table('m_kategori')->where('kategori_code', 'SNK')->update(['kategori_nama' => 'Camilan']);
+    //     // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+
+    //     // $row = DB::table('m_kategori')->where('kategori_code', 'SNK')->delete();
+    //     // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
+
+    //     $data = DB::table('m_kategori')->get();
+    //     return view('kategori', ['data' => $data]);
+    // }
 }
