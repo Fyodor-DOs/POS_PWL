24c9b23c84b39eab98a8a9862d7f0c37e5daec62
Ahmed Fathir Syafaat <fathir123t@gmail.com>
2024-03-27 23:36:23 +0700

    Tugas-Jobsheet-6

--- app/Models/m_user.php
+++ app/Models/m_user.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class m_user extends Model
 {
@@ -15,4 +18,9 @@ class m_user extends Model
         'user_id', 'level_id', 'username',
         'nama', 'password',
     ];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
